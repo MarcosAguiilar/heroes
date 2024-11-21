96d037a2464a1e2eec94ce9d7cf908ff551d2b27
Marcos Aguilar <marcosaguilar2001mal@gmail.com>
2024-11-21 22:14:30 +0100

    swagger added

---
+++ springboot/src/main/java/com/ejercico/springboot/config/SpringFoxConfig.java
@@ -0,0 +1,40 @@
+package com.ejercico.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "My Rest Hero app",
+                "Base de datos de SHIELD",
+                "API TOS",
+                "terms of service",
+                new Contact("Marcos", "https://www.linkedin.com/in/marcos-aguiilar-lozano/", "marcosaguilar2001mal@gmail.com"),
+                "Licenced of API", "Api licenced URL", Collections.emptyList()
+        );
+    }
+}
